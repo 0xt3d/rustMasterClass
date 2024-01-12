a1c52ef87df354a89d5cde23eb1c13810a1889ab
Aditya Patil <warmachine@Adityas-MacBook-Pro.local>
2024-01-12 23:14:53 +0530

    Added Block Structure

--- block.rs
+++ block.rs
@@ -1,3 +1,11 @@
 use std::timeSystemTime;
 use crypto_hash::{hex_digest, Algorithm};
 
+#[derive(Debug)]
+struct Block{
+    index: u32,
+    timestamp: u64,
+    data: String,
+    pervious_hash: String,
+    hash: String,
+}
