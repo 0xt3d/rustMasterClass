2e607bafad6af70d425de53d3cc485aafb93ea12
0xt3d <Aditya.Patil0404@gmail.com>
2024-01-22 23:31:48 +0530

    Added Function to get int

--- cal/src/main.rs
+++ cal/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Enter an integer:");
+
+  let mut input = String::new();
+
+  io::stdin()
+      .read_line(&mut input)
+      .expect("Failed to read line");
+
+  let input: i32 = match input.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+          println!("Invalid integer!");
+          return;
+      }
+  };
+
+  println!("You entered: {}", input);
 }
